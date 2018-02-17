d5daca789668829f1cb3eba05c70633d09bc3dd0
Rodnier Borrego <rodnierbc89@gmail.com>
2018-02-16 17:13:29 -0800

    create some functionalities on doctor-lookup-interface

--- assets/js/doctor-lookup-interface.js
+++ assets/js/doctor-lookup-interface.js
@@ -1,6 +1,39 @@
-import {DoctorLookup} from './../assets/js/doctor-lookup.js';
+import {doctorQuery} from './../assets/js/doctor-lookup.js';
 
-$(document).ready(function(){
-  let doctorLookup = new DoctorLookup("OR","","","");
-  doctorLookup.getDoctors();
+$(document).ready(function () {
+    let state = "or";
+    let medicalIssue = "rash";
+		let doctorsCall = doctorQuery(state, medicalIssue);
+
+		doctorsCall.then(function (response) {
+      var doctorsFounds = [];
+			let data = JSON.parse(response);
+      let body = JSON.parse(response);
+      for (var i = 0; i < body.data.length; i++) {
+        let languages = "";
+        let acceptsNewPatients = "";
+        for (var j = 0; j < body.data[i].practices.length; j++) {
+          acceptsNewPatients = body.data[i].practices[j].accepts_new_patients;
+          //alert(acceptsNewPatients);
+        }
+          let firstName = body.data[i].profile.first_name;
+          let lastName = body.data[i].profile.last_name;
+          let gender = body.data[i].profile.gender;
+          let imageUrl = body.data[i].profile.image_url;
+          for (var k = 0; k < body.data[i].profile.languages.length; k++) {
+            languages = languages + body.data[i].profile.languages[k].name + " ";
+
+          }
+          doctorsFounds.push({firstName:firstName,lastName:lastName,gender:gender,languages:languages,acceptsNewPatients:acceptsNewPatients });
+
+      }
+
+     for(i=0;i<doctorsFounds.length;i++){
+          alert(doctorsFounds[i].firstName + "  "  + doctorsFounds[i].acceptsNewPatients + " " + doctorsFounds[i].languages);
+      }
+      alert(doctorsFounds.length);
+		}, function (error) {
+			$('.showErrors').text(`There was an error processing your request: ${error.message}`);
+		});
+
 });
--- assets/js/doctor-lookup.js
+++ assets/js/doctor-lookup.js
@@ -1,53 +1,17 @@
-import apiKey from './../../.env';
+const apiKey = require('./../../.env').apiKey;
 
-export class DoctorLookup {
-
-  constructor(state, city, medicalIssue, doctorName, gender) {
-    this.state = state;
-    this.city = city;
-    this.medicalIssue = medicalIssue;
-    this.doctorName = doctorName;
-    this.gender = gender;
-  }
-
-  getDoctors() {
-    let that = this;
-      let doctorsCall = new Promise(function(resolve, reject) {
-        let request = new XMLHttpRequest();
-        let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=${that.state}&user_key=61ec296be9379d897b9fc99941d2fe6d`;
-        console.log(url);
-        request.onload = function() {
-          if (this.status === 200) {
-            resolve(request.response);
-          } else {
-            reject(Error(request.statusText));
-          }
-        };
-        request.open("GET", url, true);
-        request.send();
-      });
-
-      doctorsCall.then(function(response) {
-        let body = JSON.parse(response);
-        for (var i = 0; i < body.data.length; i++) {
-           alert(i);
-          }
-      });
-  }
-
-
-
-  get doctorName() {
-        return this._doctorName.toUpperCase();
-  }
-  set doctorName(newDoctorName) {
-      this._doctorName = newDoctorName;
-  }
-  get medicalIssue() {
-        return this._medicalIssue.toUpperCase();
-  }
-  set medicalIssue(newMedicalIssue) {
-      this._medicalIssue = newMedicalIssue;
-  }
-
-}
+export let doctorQuery = function (state, medicalIssue) {
+	return new Promise(function (resolve, reject) {
+		let request = new XMLHttpRequest();
+		let url = `https://api.betterdoctor.com/2016-03-01/doctors?location=${state}&query=${medicalIssue}&user_key=${apiKey}`
+		request.onload = function () {
+			if (this.status === 200) {
+				resolve(request.response);
+			} else {
+				reject(Error(request.statusText));
+			}
+		}
+		request.open("GET", url, true);
+		request.send();
+	});
+};
